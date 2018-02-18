39ca8e0d710f652ab7554ee96c5375c0edd7582f
JEphron <jephron63@gmail.com>
2018-02-18 22:28:16 +0100

    'Get' subcommand working

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,10 @@ extern crate serde;
 extern crate serde_derive;
 extern crate termion;
 
-use clap::{AppSettings, Arg, ArgMatches};
+use clap::{AppSettings, ArgMatches};
 use reqwest::Url;
-use serde::{Deserialize, Deserializer};
-use std::collections::HashMap;
 use std::fs;
 use std::io::{stdin, stdout, Stdout, Write};
-use std::io::Read;
 use std::str::FromStr;
 use termion::clear;
 use termion::color;
@@ -24,7 +21,7 @@ use termion::raw::RawTerminal;
 
 fn main() {
     match parse_args().subcommand() {
-        ("edit", Some(matches)) => do_edit(),
+        ("edit", Some(_)) => do_edit(),
         ("get", Some(matches)) => do_get(matches),
         _ => {}
     }
@@ -46,54 +43,81 @@ fn parse_args<'a>() -> ArgMatches<'a> {
     ).get_matches()
 }
 
-fn do_edit() {}
+fn do_edit() { panic!("not implemented"); }
 
 fn do_get(args: &ArgMatches) {
     let requested_templates = get_requested_templates(args);
-    match requested_templates {
-        Some(templates) => download_gitignore(templates),
+    let downloaded: Option<String> = match requested_templates {
+        Some(templates) => download_gitignore(templates).expect("couldn't download gitignore").into(),
         None => interactive_get()
     };
 
-    merge_or_create_gitignore()
+    if let Some(gitignore) = downloaded {
+        merge_or_create_gitignore(gitignore);
+    } else {
+        println!("no action taken");
+    }
 }
 
-fn download_gitignore(template_ids: Vec<String>) {
+fn download_gitignore(template_ids: Vec<String>) -> Result<String, reqwest::Error> {
     println!("downloading templates {:?}", template_ids);
     let base_url = "https://www.gitignore.io/api/";
     let ids = template_ids.join(",");
     let url = Url::from_str(&format!("{}{}", base_url, ids)).unwrap();
-    let gitignore = reqwest::get(url)
-        .expect("couldn't get the gitignore")
-        .text()
-        .expect("couldn't read the gitignore");
-    println!("{:?}", gitignore);
+    let result = reqwest::get(url)?.text()?;
+    Ok(result)
 }
 
-fn interactive_get() {
+fn interactive_get() -> Option<String> {
     println!("fetching list of possible keywords...");
     let ids = get_possible_ids();
-//    println!("{:?}",ids);
+    let choices = interactive_choose_ids(ids);
+    if choices.is_empty() {
+        println!("nothing selected");
+        return None;
+    }
+    let gitignore = download_gitignore(choices).expect("couldn't download gitignore");
+    Some(gitignore)
+}
+
+fn interactive_choose_ids(ids: Vec<String>) -> Vec<String> {
     let mut selections = Vec::new();
+    let mut continue_with = None;
+
+    fn print_desc(stdout: &mut RawTerminal<Stdout>, selections: &Vec<String>) -> Result<(), std::io::Error> {
+        let mut i = 1..;
+        write!(stdout, "{}{}selected: {}", clear::All, cursor::Goto(1, i.next().unwrap()), selections.join(", "))?;
+        write!(stdout, "{}keep typing to add more", cursor::Goto(1, i.next().unwrap()))?;
+        write!(stdout, "{}press backspace to delete", cursor::Goto(1, i.next().unwrap()))?;
+        write!(stdout, "{}press return to accept the current set.", cursor::Goto(1, i.next().unwrap()))?;
+        stdout.flush()?;
+        Ok(())
+    }
+
     loop {
-        match fuzzy_choose(&ids) {
+        // todo: pass the set difference between 'ids' and 'selections'
+        match fuzzy_choose(&ids, &continue_with) {
             Some(id) => {
                 selections.push(id);
-                let displayable: Vec<String> = selections.iter().cloned().map(|x| x.1).collect();
-                println!("selected: {:?}", displayable.join(", "));
-                println!("keep typing to add more");
-                println!("press backspace to delete");
-                println!("press return to accept the current set.");
                 let stdin = stdin();
                 let mut stdout = stdout().into_raw_mode().unwrap();
-                stdout.flush();
+                print_desc(&mut stdout, &selections).unwrap();
                 let mut stdin = stdin.lock().keys();
-                match stdin.next().unwrap().unwrap() {
-                    Key::Char('\n') => { break; }
-                    Key::Backspace => { selections.pop(); },
-                    Key::Char(' ') => {},
-                    _ => {}
+                loop {
+                    match stdin.next().unwrap().unwrap() {
+                        Key::Char('\n') => {
+                            return selections;
+                        }
+                        Key::Backspace => { selections.pop(); }
+                        Key::Char(c) => {
+                            continue_with = Some(c.to_string());
+                            break;
+                        }
+                        _ => continue
+                    }
+                    print_desc(&mut stdout, &selections).unwrap();
                 }
+                write!(stdout, "{}{}", cursor::Goto(1, 1), clear::All).unwrap();
             }
             None => {
                 println!("no selection made");
@@ -101,17 +125,18 @@ fn interactive_get() {
             }
         }
     }
+    selections
 }
 
-fn fuzzy_choose(available: &Vec<String>) -> Option<(usize, String)> {
+fn fuzzy_choose(available: &Vec<String>, continue_with: &Option<String>) -> Option<String> {
     fn display_matches(stdout: &mut RawTerminal<Stdout>,
                        search_string: &String,
                        items_to_display: &Vec<(usize, String)>,
                        selection: Option<usize>) {
         write!(stdout, "{}", clear::All).unwrap();
         for (i, tup) in items_to_display.iter().enumerate() {
-            let &(match_start_pos, ref string) = tup;
-            if Some(i) == selection {
+            let &(_, ref string) = tup;
+            if i == selection.unwrap_or(0) {
                 write!(stdout, "{}{}{}> {}{}{}",
                        cursor::Goto(1, (i + 2) as u16),
                        color::Bg(color::White),
@@ -125,7 +150,7 @@ fn fuzzy_choose(available: &Vec<String>) -> Option<(usize, String)> {
             }
         }
         write!(stdout, "{} search_str:: {}", cursor::Goto(1, 1), search_string).unwrap();
-        stdout.flush();
+        stdout.flush().unwrap();
     }
 
     fn find_matches(available: &Vec<String>, search_string: &String) -> Vec<(usize, String)> {
@@ -141,14 +166,14 @@ fn fuzzy_choose(available: &Vec<String>) -> Option<(usize, String)> {
 
     let stdin = stdin();
     let mut stdout = stdout().into_raw_mode().unwrap();
-    stdout.flush();
+    stdout.flush().unwrap();
 
-    let mut selection = None;
-    let mut search_string = String::new();
+    let selection;
+    let mut search_string = continue_with.clone().unwrap_or_default();
     let mut keys = stdin.lock().keys();
     let mut interior_selection: Option<usize> = None;
     loop {
-        let mut items_to_display = find_matches(available, &search_string);
+        let items_to_display = find_matches(available, &search_string);
         display_matches(&mut stdout, &search_string, &items_to_display, interior_selection);
 
         let key = keys.next().unwrap().unwrap();
@@ -166,9 +191,7 @@ fn fuzzy_choose(available: &Vec<String>) -> Option<(usize, String)> {
                 search_string.pop();
             }
             Key::Down => {
-                if interior_selection.is_none() {
-                    interior_selection = Some(0);
-                } else if interior_selection.unwrap() == items_to_display.len() - 1 {
+                if interior_selection.unwrap_or_default() == items_to_display.len() - 1 {
                     interior_selection = Some(0);
                 } else {
                     interior_selection = Some(interior_selection.unwrap_or_default() + 1);
@@ -177,24 +200,22 @@ fn fuzzy_choose(available: &Vec<String>) -> Option<(usize, String)> {
             Key::Up => {
                 if items_to_display.len() == 0 {
                     interior_selection = None;
-                } else if interior_selection.is_none() {
-                    interior_selection = Some(0);
-                } else if interior_selection.unwrap() == 0 {
+                } else if interior_selection.unwrap_or_default() == 0 {
                     interior_selection = Some(items_to_display.len() - 1);
                 } else {
                     interior_selection = Some(interior_selection.unwrap_or_default() - 1);
                 }
             }
             Key::Ctrl('c') => {
-                write!(stdout, "{}{}", cursor::Goto(1, 1), clear::All);
+                write!(stdout, "{}{}", cursor::Goto(1, 1), clear::All).unwrap();
                 return None;
             }
             _ => {}
         };
     }
 
-    write!(stdout, "{}{}", cursor::Goto(1, 1), clear::All);
-    selection
+    write!(stdout, "{}{}", cursor::Goto(1, 1), clear::All).unwrap();
+    selection.map(|x| x.1)
 }
 
 fn get_possible_ids() -> Vec<String> {
@@ -210,7 +231,7 @@ fn get_possible_ids() -> Vec<String> {
         .collect()
 }
 
-fn merge_or_create_gitignore() {
+fn merge_or_create_gitignore(gitignore: String) {
     if current_directory_has_gitignore() {
         println!(".gitignore detected in working directory");
         let should_merge = read_yes_or_no("merge? [Y/n]");
@@ -221,7 +242,11 @@ fn merge_or_create_gitignore() {
         }
     } else {
         // create
-        println!("no gitignore found in current directory");
+        let mut file = fs::File::create("./.gitignore").expect("couldn't create file");
+        match file.write_all(gitignore.as_bytes()) {
+            Ok(()) => println!("created .gitignore"),
+            Err(e) => eprintln!("{}", e)
+        }
     }
 }
 
@@ -252,14 +277,14 @@ fn paths_list_contains_gitignore(paths: fs::ReadDir) -> bool {
 fn read_yes_or_no(msg: &str) -> bool {
     let stdin = stdin();
     let mut stdout = stdout().into_raw_mode().unwrap();
-    write!(stdout, "{}", msg);
-    stdout.flush();
+    write!(stdout, "{}", msg).unwrap();
+    stdout.flush().unwrap();
     let key = stdin.keys().next().unwrap().unwrap();
     let result = match key {
         Key::Char('y') | Key::Char('Y') | Key::Char('\n') => true,
         _ => false
     };
-    write!(stdout, "{}", cursor::Right(0));
+    write!(stdout, "{}", cursor::Right(0)).unwrap();
     result
 }
 
@@ -276,17 +301,17 @@ fn get_requested_templates(matches: &ArgMatches) -> Option<Vec<String>> {
 }
 
 
-fn get_all_templates() -> Result<RemoteTemplates, reqwest::Error> {
-    let url = "https://www.gitignore.io/api/list?format=json";
-    let templates = match reqwest::get(url) {
-        Ok(mut response) => match response.json() {
-            Ok(json) => json,
-            Err(err) => panic!("couldn't parse the response. Response: {:?}\nError: {}", response, err)
-        },
-        Err(err) => panic!("couldn't get the url: {}\nError: {}", url, err),
-    };
-    Ok(templates)
-}
+//fn get_all_templates() -> Result<RemoteTemplates, reqwest::Error> {
+//    let url = "https://www.gitignore.io/api/list?format=json";
+//    let templates = match reqwest::get(url) {
+//        Ok(mut response) => match response.json() {
+//            Ok(json) => json,
+//            Err(err) => panic!("couldn't parse the response. Response: {:?}\nError: {}", response, err)
+//        },
+//        Err(err) => panic!("couldn't get the url: {}\nError: {}", url, err),
+//    };
+//    Ok(templates)
+//}
 
 #[derive(Debug, Deserialize, Clone)]
 struct TemplateData {
@@ -295,33 +320,33 @@ struct TemplateData {
     name: String,
 }
 
-type RemoteTemplates = HashMap<String, TemplateData>;
-
-mod gitignore {
-    #[derive(Debug, Serialize)]
-    struct Gitignore {
-        content_groups: Vec<Group>
-    }
-
-    #[derive(Debug, Serialize)]
-    struct Group {
-        header_text: String,
-        origin: Origin,
-        lines: Vec<(Line, Origin)>,
-    }
-
-    #[derive(Debug, Serialize, Clone, Eq, PartialEq)]
-    enum Line {
-        Whitespace,
-        Comment { text: String },
-        Entry { text: String },
-    }
-
-    #[derive(Debug, Serialize, PartialEq, Eq, Hash, Clone)]
-    enum Origin {
-        Local,
-        Remote { id: String },
-        // id is the gitignore.io id for lines and groups
-        Unknown, // origin will be unknown until we get a chance to compare against the remote file. Should we default to Local?
-    }
-}
+//type RemoteTemplates = HashMap<String, TemplateData>;
+//
+//mod gitignore {
+//    #[derive(Debug, Serialize)]
+//    struct Gitignore {
+//        content_groups: Vec<Group>
+//    }
+//
+//    #[derive(Debug, Serialize)]
+//    struct Group {
+//        header_text: String,
+//        origin: Origin,
+//        lines: Vec<(Line, Origin)>,
+//    }
+//
+//    #[derive(Debug, Serialize, Clone, Eq, PartialEq)]
+//    enum Line {
+//        Whitespace,
+//        Comment { text: String },
+//        Entry { text: String },
+//    }
+//
+//    #[derive(Debug, Serialize, PartialEq, Eq, Hash, Clone)]
+//    enum Origin {
+//        Local,
+//        Remote { id: String },
+//        // id is the gitignore.io id for lines and groups
+//        Unknown, // origin will be unknown until we get a chance to compare against the remote file. Should we default to Local?
+//    }
+//}
